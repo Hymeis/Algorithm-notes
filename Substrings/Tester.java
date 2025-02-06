62dadfc99bd113092d8e64aa44710524baf4f505
小狼 <44147445+Hymeis@users.noreply.github.com>
2025-02-05 20:17:54 -0800

    doc: modified comments

--- Substrings/Tester.java
+++ Substrings/Tester.java
@@ -1,19 +1,15 @@
 package Substrings;
 
+/**
+ * Author: Hymeis
+ * Date Created: Feb. 5, 2025
+ * Program description: The driver for testing the algorithms and
+ * functions
+ */
 public class Tester {
-
-    /**
-     * Return the substring (shorter) index if it exists in longer. Return -1 is shorter is
-     * not a substring of longer
-     * @param longer The longer string
-     * @param shorter The shorter string
-     * @return The index of shorter within longer
-     */
 
     public static void main(String[] args) {
         Util utilTester = new Util();
-
-        // A set of test cases: each array has [longer, shorter].
         String[][] testCases = {
             {"Hymeis", "ei"},
             {"Hello World", "World"},
@@ -26,19 +22,11 @@ public class Tester {
         for (String[] pair : testCases) {
             String longer = pair[0];
             String shorter = pair[1];
-
-            // Expected result using the built-in indexOf
             int expectedIndex = longer.indexOf(shorter);
-
-            // Actual result using your custom indexOf
             int actualIndex = utilTester.indexOf(longer, shorter);
-
-            // Compare the two results
             boolean testPassed = (expectedIndex == actualIndex);
-
-            // Print results: True if they match, False otherwise
             System.out.printf(
-                "Longer: \"%s\" | Shorter: \"%s\" |\n\t Expected: %d | Actual: %d | Test Passed: %b%n",
+                "Longer: \"%s\" | Shorter: \"%s\" \n\tExpected: %d | Actual: %d \n\tTest Passed: %b%n",
                 longer, shorter, expectedIndex, actualIndex, testPassed
             );
         }
--- Substrings/Util.java
+++ Substrings/Util.java
@@ -1,17 +1,24 @@
 package Substrings;
 
+/**
+ * Author: Hymeis
+ * Date Created: Feb. 5, 2025
+ * Program description: A set of functions that applies substring algorithms
+ */
 public class Util {
     private ZAlgorithm zAlg = new ZAlgorithm();
 
-    
+
     /**
      * Return the substring (shorter) index if it exists in longer. Return -1 is shorter is
      * not a substring of longer
+     * @pre Assume longer and shorter only contains alphabetical letters (must not contain '#')
      * @param longer The longer string
      * @param shorter The shorter string
      * @return The index of shorter within longer
      */
     public int indexOf(String longer, String shorter) {
+        // Separate longer and shorter
         String temp = shorter + '#' + longer;
         int[] z = zAlg.zArray(temp);
         for (int i = shorter.length() + 1; i < z.length; i++) {
